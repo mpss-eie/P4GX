3fd65753f204daac97bb388dd959813c45022801
Fabián Abarca <ensinergia@gmail.com>
2022-11-01 18:35:56 -0600

    Modificación de la frase de la función de creación de una gráfica tridimensional.

--- proceso/proceso.py
+++ proceso/proceso.py
@@ -1,11 +1,14 @@
 def demanda():
     return 'Aquí están los datos.'
 
+
 def densidad():
     return 'Aquí está la densidad de probabilidad.'
 
+
 def grafica():
-    return 'Aquí está la gráfica.'
+    return 'Aquí está la gráfica tridimensional.'
+
 
 def probabilidad():
     return 'Aquí está la probabilidad.'
